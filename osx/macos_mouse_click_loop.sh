22f08fde020ebb31f59402ac7a39e9db954c9c68
Lee Blackburn <g.lee.blackburn@gmail.com>
2026-04-21 01:47:37 -0400

    add other buy clicks

--- osx/macos_mouse_click_loop.sh
+++ osx/macos_mouse_click_loop.sh
@@ -19,6 +19,7 @@ while (true) ; do
     # uncomment export lines for debug output
     export MACOS_MOUSE_CLICK_DEBUG_TUI=yes
     export MACOS_MOUSE_CLICK_DEBUG_TUI_LOG=debug.json
+
     echo "buy alchemy lab"
     ${mouse_click} -d 0 -x 2344.4 -y -103.6 -n 5 -Y
     echo "buy shipment"
@@ -27,6 +28,19 @@ while (true) ; do
     ${mouse_click} -d 0 -x 2344.4 -y -237.3 -n 5 -Y
     echo "buy temple"
     ${mouse_click} -d 0 -x 2344.4 -y -304.6 -n 5 -Y
+    echo "buy bank"
+    ${mouse_click} -d 0 -x 2344.4 -y -368.1 -n 5 -Y
+    echo "buy factory"
+    ${mouse_click} -d 0 -x 2344.4 -y -430.9 -n 5 -Y
+    echo "buy mine"
+    ${mouse_click} -d 0 -x 2344.4 -y -495.6 -n 5 -Y
+    echo "buy farm"
+    ${mouse_click} -d 0 -x 2344.4 -y -558.5 -n 5 -Y
+    echo "buy grandma"
+    ${mouse_click} -d 0 -x 2344.4 -y -623.4 -n 5 -Y
+    echo "buy cursor"
+    ${mouse_click} -d 0 -x 2344.4 -y -689.3 -n 5 -Y
+
     echo "click the cookie"
     ${mouse_click} -d 0 -x 1600.8 -y -410.9 -n 3000 -Y
     date
